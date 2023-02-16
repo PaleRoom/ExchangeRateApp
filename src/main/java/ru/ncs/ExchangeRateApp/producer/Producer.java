ac0ab25e2f614d998652cf46cf99c0992af5bd6f
Nicolas <98647243+PaleRoom@users.noreply.github.com>
2023-02-16 12:15:45 +0400

    Initial

--- src/main/java/ru/ncs/ExchangeRateApp/producer/Producer.java
+++ src/main/java/ru/ncs/ExchangeRateApp/producer/Producer.java
@@ -6,7 +6,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import ru.ncs.ExchangeRateApp.response.ExchangeResponse;
 
-@Service
+//@Service
 @RequiredArgsConstructor
 @Slf4j
 public class Producer {
--- src/main/java/ru/ncs/ExchangeRateApp/web/health/HealthController.java
+++ src/main/java/ru/ncs/ExchangeRateApp/web/health/HealthController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
+
     @Value("${app.healthCheckMsg}")
     private String msg;
 
